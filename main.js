fd3877df3d1c85d51521c1af20fb0a4569897355
María Viriginia castillo <127612492+vircastillo75@users.noreply.github.com>
2024-06-29 16:40:57 -0300

    cambios en index html y en responsives terminados

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const hamburgerMenu = document.querySelector('#hamburger-menu');
-const navbar = document.querySelector('#navbar');
-const closeBtn = document.querySelector('#close-btn')
+const closeBtn = document.querySelector('#close-btn');
+const navbar = document.getElementById('navbar')
 
 hamburgerMenu.addEventListener('click', ()=>{
     navbar.style.display ='block'
